590c324e5c6ec27710bdebf2678ee3f17ece3796
Cristian Yalanda <cristiandavid20007@gmail.com>
2026-09-13 15:10:14 -0500

    Inicio de sesion

---
+++ inicioSesion.js
@@ -0,0 +1,5 @@
+// Esta réplica es solo visual - reproduce el diseño exacto del login,
+// pero no tiene lógica de registro/inicio de sesión conectada (los botones
+// e inputs no hacen nada todavía).
+// Se necesita el registro para conectarlos con js pero como no hay registro aun pues no se le hace
+// Cuando hagan el registro ahi si ya funcionarian los botones ya que todo esta simplemente visualmente
